83beddda9bc7b32f86e83247d0b192a0a36db7ed
Sami <samihadouaj@gmail.com>
2020-07-25 11:07:24 +0100

    exctracted the adding logic into a separate method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
+    public static int add(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-        int z = a+b;
+        int z = add(a,b);
         System.out.println("a+b = "+z);
     }
 }
